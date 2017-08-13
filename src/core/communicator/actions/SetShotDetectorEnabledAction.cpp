92cb507cb68921a7d28566a8635e60dcc20f01ed
igor <ifedorenko@gmail.com>
2017-08-13 22:44:52 +0400

    Багфикс: детектор теперь можно выключить

--- src/core/communicator/actions/SetShotDetectorEnabledAction.cpp
+++ src/core/communicator/actions/SetShotDetectorEnabledAction.cpp
@@ -10,11 +10,8 @@ bool SetShotDetectorEnabledAction::match(String command) {
     if (command.startsWith("detector:enabled:")) {
         String copy = command;
         copy.remove(0, 17);
-        int level = copy.toInt();
-        if (level > 0) {
-            value = level;
-            return true;
-        }
+        value = copy.toInt();
+        return true;
     }
     return false;            
 }
